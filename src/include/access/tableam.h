106b469b705109f7ba95206978f90cad2b72bf24
Nikita Malakhov <n.malakhov@postgrespro.ru>
2022-07-22 10:41:41 +0300

    Default TOAST re-implemented using TOAST API.
    Default TOAST mechanics extracted from Heap AM Core and
    re-implemented using Pluggable TOAST API as 'Default Toaster'.
    Default toaster is added as initial entry in PG_TOASTER table,
    with Toaster name 'deftoaster' and still is part of PostgreSQL
    core.
    
    Overall TOAST mechanics is kept the same, the difference is
    that from now on TOAST table initialization is done within
    Generic Toaster "init" functions, toast and detoast functions
    are called as Toaster with DEFAULT_TOASTER_OID via TOAST API.
    Default Toaster is attached to TOASTable column by default if
    not stated otherwise (by CREATE or ALTERD TABLE command option
    SET TOASTER). Default Toaster could be replaced with custom
    toaster by SET TOASTER clause in CREATE TABLE or ALTER TABLE
    command.
    
    Author: Teodor Sigaev <teodor@sigaev.ru>
    Author: Oleg Bartunov <obartunov@postgrespro.ru>
    Author: Nikita Glukhov <n.gluhov@postgrespro.ru>
    Author: Nikita Malakhov <n.malakhov@postgrespro.ru>
    
    Discussion: https://www.postgresql.org/message-id/flat/224711f9-83b7-a307-b17f-4457ab73aa0a@sigaev.ru
    Signed-off-by: Greg Burd <gregburd@amazon.com>

--- src/include/access/tableam.h
+++ src/include/access/tableam.h
@@ -742,6 +742,7 @@ typedef struct TableAmRoutine
 	 * for more details.
 	 */
 	void		(*relation_fetch_toast_slice) (Relation toastrel, Oid valueid,
+											   struct varlena *attr,
 											   int32 attrsize,
 											   int32 sliceoffset,
 											   int32 slicelength,
@@ -1912,10 +1913,12 @@ table_relation_toast_am(Relation rel)
  */
 static inline void
 table_relation_fetch_toast_slice(Relation toastrel, Oid valueid,
+								 struct varlena *attr,
 								 int32 attrsize, int32 sliceoffset,
 								 int32 slicelength, struct varlena *result)
 {
 	toastrel->rd_tableam->relation_fetch_toast_slice(toastrel, valueid,
+													 attr,
 													 attrsize,
 													 sliceoffset, slicelength,
 													 result);
