73e7adc5bd267b8971b431d9b72b53c7274a9ea0
Gregory Burd <gregburd@amazon.com>
2025-01-27 13:28:59 -0500

    Expand HOT update path to include expression and partial indexes.
    This patch extends the cases where HOT updates are possible in the heapam by
    examining expression indexes and determining if indexed values where mutated
    or not.  Previously, any expression index on a column would disqualify it
    from the HOT update path. Also examines partial indexes to see if the
    values are within the predicate or not.
    
    This is a modified application of a patch proposed on the pgsql-hackers list:
    https://www.postgresql.org/message-id/flat/4d9928ee-a9e6-15f9-9c82-5981f13ffca6%40postgrespro.ru
    applied: https://git.postgresql.org/gitweb/?p=postgresql.git;a=commit;h=c203d6cf8
    reverted: https://git.postgresql.org/gitweb/?p=postgresql.git;a=commit;h=05f84605dbeb9cf8279a157234b24bbb706c5256
    
    Signed-off-by: Greg Burd <gregburd@amazon.com>

--- src/backend/replication/logical/worker.c
+++ src/backend/replication/logical/worker.c
@@ -2861,7 +2861,7 @@ apply_handle_update_internal(ApplyExecutionData *edata,
 	MemoryContext oldctx;
 
 	EvalPlanQualInit(&epqstate, estate, NULL, NIL, -1, NIL);
-	ExecOpenIndices(relinfo, false);
+	ExecOpenIndicesForUpdate(relinfo, false, true);
 
 	found = FindReplTupleInLocalRel(edata, localrel,
 									&relmapentry->remoterel,
