6511c4ae596ccd3e8762db0a614022c7ab2bb1ab
Greg Burd <greg@burd.me>
2025-07-02 07:17:55 -0400

    dev setup v8

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "PostgreSQL development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        shellConfig = import ./shell.nix {inherit pkgs system;};
+      in {
+        formatter = pkgs.alejandra;
+        devShells = {
+          default = shellConfig.devShell;
+          gcc = shellConfig.devShell;
+          clang = shellConfig.clangDevShell;
+          gcc-musl = shellConfig.muslDevShell;
+          clang-musl = shellConfig.clangMuslDevShell;
+        };
+
+        packages = {
+          inherit (shellConfig) gdbConfig flameGraphScript pgbenchScript;
+        };
+
+        environment.localBinInPath = true;
+      }
+    );
+}
